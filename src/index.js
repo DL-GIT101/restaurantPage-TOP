da9e9b99ce602e615eb0243acf8641ff78e4b31b
donlHenard <pastor.git101@gmail.com>
2023-08-13 01:03:32 +0800

    Imported landing-package
    I removed the content div from the html cause of the plugin. no choice
    in this case.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-import { landingPage } from './landing-page';
+import  landingPage from './landing-page';
 import './style.css';
+
+const createContent =  () => {
+    const content = document.createElement('div');
+    content.setAttribute("id","content");
+    document.body.appendChild(content);
+}
+
+createContent();
+landingPage();
+
+/* 
+       <div class="about">
+            <h2>Meals provided by nature</h2>
+            <img src="../src/restaurant-view.jpg" alt="Restaurant View">
+       </div>
+        <div class="testimonies">
+            <div class="card">
+                <p>"Superb menu"</p>
+                <img src="../src/testimony-1.jpg" alt="Person">
+            </div>
+            <div class="card">
+                <p>"You can tase freshness"</p>
+                <img src="../src/testimony-2.jpg" alt="Person">
+            </div>
+            <div class="card">
+                <p>"Be one with nature"</p>
+                <img src="../src/testimony-3.jpg" alt="Person">
+            </div>
+*/
