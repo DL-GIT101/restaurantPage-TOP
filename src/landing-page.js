22cdf1315174f59047172a962eeccdaa5c18e478
donlHenard <pastor.git101@gmail.com>
2023-08-13 01:02:01 +0800

    Add fn for nav making and landingpage
    note its on testing phase as i tried many things

--- src/landing-page.js
+++ src/landing-page.js
@@ -1,3 +1,26 @@
-export const landingPage = () => {
+const navMake = () => {
+    const nav = document.createElement('nav');
+    //logo name
+    const restaurantName = document.createElement('h1');
+    restaurantName.innerHTML = "The Green Lifestyle";
+    //tabs
+    const tabs = document.createElement('ol');
+    const tabsArray = ["Menu", "About", "Contact"];
+    tabsArray.forEach(tab => {
+        const li = document.createElement('li');
+        li.innerHTML = tab;
+        li.setAttribute("id", tab.toLocaleLowerCase());
+        tabs.appendChild(li);
+    });
 
+    nav.append(restaurantName,tabs);
+    return nav;
 }
+
+const landingPage = () => {
+    const content = document.getElementById("content");
+    const nav = navMake();
+    content.appendChild(nav);
+}
+
+export default landingPage;
