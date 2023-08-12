b272a047eaed97f5dc50015ee4bf25f4dce662a1
donlHenard <pastor.git101@gmail.com>
2023-08-13 00:59:53 +0800

    Change to mode dev & add html plugin
    also added inline source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+      },
+    devtool: 'inline-source-map',
+    plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Restaurant Page',
+    }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
   module: {
     rules: [
       {
