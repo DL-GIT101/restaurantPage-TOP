61f038970fc99e35f96330f7977cc4e6848ce552
donlHenard <pastor.git101@gmail.com>
2023-08-13 18:34:16 +0800

    Deleted the comment

--- src/index.js
+++ src/index.js
@@ -17,23 +17,3 @@ const initialLoad = () => {
 }
 
 initialLoad();
-
-/* 
-       <div class="about">
-            <h2>Meals provided by nature</h2>
-            <img src="../src/restaurant-view.jpg" alt="Restaurant View">
-       </div>
-        <div class="testimonies">
-            <div class="card">
-                <p>"Superb menu"</p>
-                <img src="../src/testimony-1.jpg" alt="Person">
-            </div>
-            <div class="card">
-                <p>"You can tase freshness"</p>
-                <img src="../src/testimony-2.jpg" alt="Person">
-            </div>
-            <div class="card">
-                <p>"Be one with nature"</p>
-                <img src="../src/testimony-3.jpg" alt="Person">
-            </div>
-*/
