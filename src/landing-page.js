006b13c6c7de367e701866140588eaae1eaeed55
donlHenard <pastor.git101@gmail.com>
2023-08-13 14:16:37 +0800

    Add svg on logo
    Add svg as a object then made a div for h1 and svg

--- src/landing-page.js
+++ src/landing-page.js
@@ -1,9 +1,18 @@
 const navMake = () => {
     const nav = document.createElement('nav');
-    //logo name
+//logo
+    const logo = document.createElement('div');
+    logo.className = "logo";
+    //name
     const restaurantName = document.createElement('h1');
     restaurantName.innerHTML = "The Green Lifestyle";
-    //tabs
+    //icon
+    const icon = document.createElement('object');
+    icon.setAttribute("type", "image/svg+xml");
+    icon.setAttribute("data", "../src/img/leaf.svg");
+    icon.className = "icon";
+    logo.append(restaurantName,icon);
+//tabs
     const tabs = document.createElement('ol');
     const tabsArray = ["Menu", "About", "Contact"];
     tabsArray.forEach(tab => {
@@ -13,14 +22,14 @@ const navMake = () => {
         tabs.appendChild(li);
     });
 
-    nav.append(restaurantName,tabs);
+    nav.append(logo,tabs);
     return nav;
 }
 
 const landingPage = () => {
     const body = document.querySelector("body");
     const nav = navMake();
-    content.appendChild(nav);
+    body.appendChild(nav);
 }
 
 export default landingPage;
